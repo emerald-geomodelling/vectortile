a903e1fd56f601e6d8dd841addc53b7a49831857
Egil Moeller <egil.moller@freecode.no>
2014-08-27 11:38:50 +0200

    Merged in typedmatrix

--- setup.py
+++ setup.py
@@ -4,35 +4,14 @@ from glob import glob
 from os.path import sep
 from distutils.core import setup
 
-
+import vectortile
 
 setup(name='vectortile',
-      version='1.1.1',
-      author='Paul Woods',
+      version=vectortile.__version__,
+      author=vectortile.__author__,
       author_email='paul@skytruth.org',
-      description='',
-      long_description='',
-      url='https://github.com/SkyTruth/vectortile',
-      license="""The MIT License (MIT)
-
-Copyright (c) 2014 SkyTruth
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-""",
+      description=vectortile.__doc__,
+      long_description=vectortile.__doc__,
+      url=vectortile.__source__,
+      license=vectortile.__license__,
       packages=['vectortile', 'vectortile.tests'])
